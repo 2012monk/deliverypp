3acb7df4cb03c0a281789d6129198cac4c8bb897
TAEKWON OH <otw7917@gmail.com>
2021-05-11 12:12:01 +0900

    hjhjhj

--- src/main/java/com/deli/deliverypp/model/AuthInfo.java
+++ src/main/java/com/deli/deliverypp/model/AuthInfo.java
@@ -1,1 +1,52 @@
+package com.deli.deliverypp.model;
 
+import com.deli.deliverypp.DB.DeliUser;
+
+public class AuthInfo {
+
+    private String access_token;
+    private String auth_type;
+    private DeliUser user;
+    private long exp;
+
+    public long getExp() {
+        return exp;
+    }
+
+    public void setExp(long exp) {
+        this.exp = exp;
+    }
+
+    public String getAccess_token() {
+        return access_token;
+    }
+
+    public void setAccess_token(String access_token) {
+        this.access_token = access_token;
+    }
+
+    public String getAuth_type() {
+        return auth_type;
+    }
+
+    public void setAuth_type(String auth_type) {
+        this.auth_type = auth_type;
+    }
+
+    public DeliUser getUser() {
+        return user;
+    }
+
+    public void setUser(DeliUser user) {
+        this.user = user;
+    }
+
+    @Override
+    public String toString() {
+        return "AuthInfo{" +
+                "access_token='" + access_token + '\'' +
+                ", auth_type='" + auth_type + '\'' +
+                ", user=" + user +
+                '}';
+    }
+}
--- src/main/java/com/deli/deliverypp/util/ConfigLoader.java
+++ src/main/java/com/deli/deliverypp/util/ConfigLoader.java
@@ -2,13 +2,19 @@ package com.deli.deliverypp.util;
 
 import com.deli.deliverypp.util.annotaions.KeyLoad;
 import com.deli.deliverypp.util.annotaions.LoadConfig;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.reflections.Reflections;
 
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import javax.xml.bind.DatatypeConverter;
+import java.io.*;
 import java.lang.reflect.Field;
 import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.HashMap;
 import java.util.Properties;
@@ -20,24 +26,80 @@ public class ConfigLoader {
     private static final Logger log = Logger.getGlobal();
     private final HashMap<String, Properties> cache = new HashMap<>();
 
+
     static {
         Reflections reflections = new Reflections("com.deli.deliverypp");
         Set<Class<?>> annotated = reflections.getTypesAnnotatedWith(LoadConfig.class);
         for (Class<?> c: annotated) {
             try {
                 load(c.getDeclaredAnnotation(LoadConfig.class).path(), c);
-            } catch (ClassNotFoundException e) {
+            }
+            catch (ClassNotFoundException e) {
                 log.warning("load failed");
+                e.printStackTrace();
+            }
+            catch (IllegalArgumentException e){
+                throw new IllegalArgumentException("target filed is not a static filed");
+            } catch (URISyntaxException | IOException fe) {
+                fe.printStackTrace();
+            } catch (IllegalAccessException e) {
                 e.printStackTrace();
             }
         }
     }
 
     static public void load(String path, Class<?> aClass) throws ClassNotFoundException {
+
+    static public void loadKey(String path, Field field) {
+        URL keyPath = ConfigLoader.class.getClassLoader().getResource(path);
+        System.out.println(keyPath);
+        try {
+
+            URI uri = keyPath.toURI();
+            File file = new File(uri);
+            FileReader fr = new FileReader(file);
+            BufferedReader br = new BufferedReader(fr);
+
+            StringBuilder sb = new StringBuilder();
+            String line;
+            while ((line = br.readLine() ) != null) {
+                if (!line.equals("-----BEGIN RSA PRIVATE KEY-----") && !line.equals("-----END RSA PRIVATE KEY-----")){
+                    sb.append(line);
+
+                }
+            }
+
+            byte[] keyBytes = DatatypeConverter.parseBase64Binary(sb.toString());
+            SecretKey secretKey = new SecretKeySpec(keyBytes, SignatureAlgorithm.HS256.getJcaName());
+//            System.out.println(Arrays.toString(secretKey.getEncoded()));
+            if (field.getType() == String.class) {
+                field.set(String.class, sb.toString());
+            }
+            field.set(SecretKey.class, secretKey);
+            log.info("key load complete from  "+ "[" + field.getDeclaringClass().getName() + "]");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+//    static public void parsePrivate(String key) {
+//        RSAPrivateKey k = (SecretKey)
+//    }
+
+    /**
+     *
+     * Loading configuration files *.properties and set to static variable
+     *
+     * @param path config file path
+     * @param aClass target class
+     * @throws ClassNotFoundException target class not found
+     */
+
+    static public void load(String path, Class<?> aClass) throws ClassNotFoundException, IllegalArgumentException, IOException, URISyntaxException, IllegalAccessException {
         Properties p = new Properties();
 
         URL configPath = ConfigLoader.class.getClassLoader().getResource(path);
-        try {
+//        try {
             File file = null;
             URI uri = null;
             if (configPath == null){
@@ -64,14 +126,14 @@ public class ConfigLoader {
                     }
                 }
 
-                f.set(String.class, p.getProperty(f.getName()));
+                if (f.get(f.getType()) == null){
+                    f.set(aClass, p.getProperty(f.getName()));
+                }
             }
 
             log.info( "load Complete "  +"\n"+"[" + Thread.currentThread().getName() + Thread.currentThread().getId()+ "]"+ aClass.getName() );
 
-        }catch (Exception e){
-            e.printStackTrace();
-        }
+//        }
     }
 
     public static Class<?> call() throws ClassNotFoundException {
