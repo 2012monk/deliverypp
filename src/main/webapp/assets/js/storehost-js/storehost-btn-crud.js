d33e6b006674c8e97298ec0a35bc575c0466c395
TAEKWON OH <otw7917@gmail.com>
2021-05-12 13:41:15 +0900

    1. basket.js 장바구니 중간단
    2. index-header.js 시작.. js
    
    3. index.html
    
    4. storehost-storelist.js 수정 update button id-> class
    
    5. storehost-btn-crud.js 수정 지우기, 수정폼 까지
    6. storehost.html 스크립트 추가
    7. style.css

--- src/main/webapp/assets/js/storehost-js/storehost-btn-crud.js
+++ src/main/webapp/assets/js/storehost-js/storehost-btn-crud.js
@@ -2,7 +2,7 @@ $(function(){
 	
 	s="<button id='storehost-btn-add'>추가</button>";
 	
-	$("#storehost-btn-crud").html(s);
+	$("#storehost-header").html(s);
 	
 });
 
@@ -22,7 +22,7 @@ $(document).on("click","#storehost-btn-add",function(){
 	s+="<tr><td colspan='2' align='center'><button>추가하기</button></td></tr>";
 	s+="</table>";
 	s+="</form>";
-	$("#storehost-storelist").html(s);
+	$("#storehost-header").html(s);
 	
 });
 
@@ -70,7 +70,40 @@ $(document).on("submit", "#mdform", function(e){
 $(document).on("click",".storelist-btn-delete",function(){
 	var storeId = $(this).attr("value");
 	console.log(storeId);
+	$.ajax({
+		type:"delete",
+		url:"http://112.169.196.76:47788/stores/"+storeId,
+		success:function(data){
+			console.log(data);
+		}
+	});
 });
+
+$(document).on("click","storelist-btn-update",function(){
+	var storeId = $(this).attr("value");
+	console.log(storeId);
+	//매장 등록 폼
+	var s="";
+	s+="<form id='mdform'>";
+	s+="<input type='hidden' id='storeId' value=''>";
+	s+="<table>";
+	s+="<caption><b>매장등록</b></caption>";
+	s+="<tr><td>매장명</td><td><input type='text' name='storeName' required></td><td><button id='idcheckbtn'>중복확인</button></td></tr>";
+	s+="<tr><td>매장소개</td><td><textarea name='storeDesc' required></textarea></td></tr>";
+	s+="<tr><td>매장사진</td><td><input type='text' name='storeImage' required></td></tr>";
+	s+="<tr><td>매장주소</td><td><input type='text' name='storeAddr' required></td></tr>";
+	s+="<tr><td colspan='2' align='center'><button>등록 완료</button></td></tr>";
+	s+="</table>";
+	s+="</form>";
+	
+	$("#storehost-storelist").html(s);
+	
+});
+
+
+
+
+
 
 function deleteStoreHostListBtn() {
 	/*var storeId = document.getElementById("storelist-btn-delete");*/
